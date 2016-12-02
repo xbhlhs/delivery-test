c0ace91ca3b6f158555b0d0138aa49ffee35059f
xbhlhs <xbhlhs@gmail.com>
2016-12-02 14:00:52 +0800

    init

---
+++ Test.java
@@ -0,0 +1,34 @@
+
+import java.util.List;
+
+public class Test {
+    /**
+     * <p>Sum all the minimum number of the sub-list and all the maximum number of the sub-list.</p>
+     * <p>the input may be as<br/>
+     * [<br/>
+     * [1, 2],<br/>
+     * [3, 2, 1],<br/>
+     * [4, 8, 2, 1],<br/>
+     * [1],<br/>
+     * [3, 2, 3]<br/>
+     * ]<br/>
+     * variable number of sub-lists and variable number of integers in the sub-lists.</p>
+     * <h4>Example:</h4>
+     * <p>the input:<br/>
+     * [<br/>
+     * [1, 2],<br/>
+     * [1],<br/>
+     * [1, 2, 3]<br/>
+     * ]<br/>
+     * the output:<br/>
+     * [3, 6]</p>
+     *
+     * @param numbers integer list collection.
+     * @return the result list as [sumAllMinimum, sumAllMaximum]
+     * 
+     * Note: the time complexity need to be O(n).
+     */
+    private static List<Integer> sumMinimumAndMaximum(List<List<Integer>> numbers) {
+        return null;
+    }
+}
